1bc89f05e5827edef9179d66f40ea4445f5b44b0
egomez <eduardgb92@gmail.com>
2023-09-05 23:21:18 +0200

    added forecast and day or night state

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const temperature = document.createElement("div");
 temperature.setAttribute("id", "temperature");
 const weatherCode = document.createElement("div");
 weatherCode.setAttribute("id", "weatherCode");
+const dayOrNight = document.createElement("div");
+dayOrNight.setAttribute("id", "day-or-night");
 
 weather.appendChild(temperature);
 weather.appendChild(weatherCode);
+weather.appendChild(dayOrNight);
 
 //form for the user to input location
 locationForm.addEventListener("submit", (e) => {
@@ -25,6 +28,54 @@ locationForm.addEventListener("submit", (e) => {
 
 });
 
+//turns code into the corresponding weather icon
+
+function getWeatherSymbol(weatherCode){
+    if (weatherCode == 1){
+
+    } else if(weatherCode == 2){
+
+    }
+}
+
+
+// get the data for the 7 days forecast and show it
+
+
+
+function getForecast(daysArray){
+
+    for (i=0; i<daysArray.time.length; i++){
+        const day = document.createElement("div");
+        day.className = "day";
+        const date = document.createElement("div");
+        date.className = "date";
+        date.innerHTML = daysArray.time[i];
+        const maxTemp = document.createElement("div");
+        maxTemp.className = "maxTemp";
+        maxTemp.innerHTML = daysArray.temperature_2m_max[i];
+        const minTemp = document.createElement("div");
+        minTemp.className = "minTemp";
+        minTemp.innerHTML = daysArray.temperature_2m_min[i];
+        console.log(daysArray[i])
+
+        weather.appendChild(day);
+        day.appendChild(date);
+        day.appendChild(maxTemp);
+        day.appendChild(minTemp);
+    }
+}
+
+// check if it's day or night
+
+function getDayOrNight(code){
+    if (code == 1){
+        return "day;"
+    } else {
+        return "night";
+    }
+}
+
 
     //get location coordinates
     async function getLocation(locationSearch){
@@ -42,15 +93,20 @@ locationForm.addEventListener("submit", (e) => {
     //get the weather data from the location
     async function getWeather(locationSearch){
         const coordinates = await getLocation(locationSearch);
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + coordinates[0] + '&longitude=' + coordinates[1] + '&hourly=temperature_2m&current_weather=true', {mode: 'cors'});
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + coordinates[0] + '&longitude=' + coordinates[1] + '&hourly=is_day&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FBerlin', {mode: 'cors'});
         const weatherData = await response.json();
         console.log("weather data" + JSON.stringify(weatherData));
+        console.log(weatherData);
         temperature.innerHTML = "Temperature: " + weatherData.current_weather.temperature;
         weatherCode.innerHTML = "Weather: " + weatherData.current_weather.weathercode;
+        dayOrNight.innerHTML = getDayOrNight(weatherData.current_weather.is_day);
+        const days = weatherData.daily;
+        getForecast(days)
     }
+    
 
 
-
+    
     /*
 
     0 	Clear sky
